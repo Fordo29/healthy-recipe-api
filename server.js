4792522535127c0af110330c8cbd5290ef312a67
Christine Rowland <fordo29@gmail.com>
2022-06-14 17:00:28 -0600

    Inital set up of API

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const recipes = require('./recipedata');
+
+app.set('port', process.env.PORT || 3001);
+app.locals.title = 'Heathly Eater';
+app.use(express.json());
+
+const cors = require('cors');
+app.use(cors());
+
+app.locals.foods = recipes;
+
+app.get('/', (request, response) => {
+  response.send('Welcome to the Healthy Recipe API!');
+});
+
+app.get('/api/v1/foods', (request, response) => {
+  const foods = app.locals.foods;
+  if(!foods) {
+    response.status(500).json({ error })
+  }
+  response.status(200).json({foods})
+});
+
+app.get('/api/v1/foods/:food_id', (request, response) => {
+  const foodId = parseInt(request.params.food_id);
+  const recipe = app.locals.foods.find(food => {
+    return foodId === food.id;
+  });
+  if(!recipe) {
+    return response.sendStatus(404);
+  }
+  response.status(200).json({ recipe });
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});
